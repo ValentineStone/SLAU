28631a0fe552436cbe1ef49d886523dfa7d4682b
Valentine Stone <SirValentineStone@gmail.com>
2016-03-18 21:23:58 +0500

    Removed cute art, started work on edit() function.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include "slau.hpp"
 
+int edit(slau*);
+
 int main()
 {
-    std::cout << "===============================================================\n"
-              << "|| ♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥ ||\n"
-              << "|| ♥♡♥♡♥♡♥♡♥                                       ♥♡♥♡♥♡♥♡♥ ||\n"
-              << "|| ♥♡♥♡♥♡♥♡♥ Welcome to the best SLAU solver ever! ♥♡♥♡♥♡♥♡♥ ||\n"
-              << "|| ♥♡♥♡♥♡♥♡♥                                       ♥♡♥♡♥♡♥♡♥ ||\n"
-              << "|| ♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥ ||\n"
-              << "===============================================================\n";
+    std::cout << "Welcome to the best SLAU solver ever!\n\n";
 
-    std::cout << "How many equasions we require?: ";
+    std::cout << "How many equations we require?: ";
 
     int height;
 
@@ -27,13 +23,58 @@ int main()
 
     problem->print();
 
+
+
+    std::cout << "\nWanna change it? (y/n): ";
+
+    char reply;
+    bool is_input_valid = false;
+
+    while (!is_input_valid)
+    {
+        std::cin >> reply;
+
+        switch (reply)
+        {
+            case 'y':
+            case 'Y':
+            {
+                edit(problem);
+                is_input_valid = true;
+                break;
+            }
+            case 'n':
+            case 'N':
+            {
+                is_input_valid = true;
+                break;
+            }
+            default:
+            {
+                std::cout << "Unsure what you ment, wanna change the matrix? (y/n): ";
+            }
+        }
+    }
+
+
+
     std::cout << "\nHere is how we solved it:\n\n";
 
     problem->solve();
+
+
 
     std::cout << "\nHere is how we checked it:\n\n";
 
     problem->check();
 
+
+
+    return 0;
+}
+
+int edit(slau* _problem)
+{
+    std::cout << "Changed it!\n\n";
     return 0;
 }
